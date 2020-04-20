d319996f13dfdcc5f5957b355608e71768ac6710
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-04-20 22:36:40 +0200

    comments fixed

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-//ask distance
+//ask distance - recap line only appears when filled
 var total_km = prompt('Quanti km devi percorrere?', '100');
 console.log(total_km);
 document.getElementById('given-km').innerHTML = total_km;
 document.getElementById('data-container').setAttribute('class', 'visible');
 document.getElementById('km-container').setAttribute('class', 'visible');
 
-//ask age
+//ask age - recap line only appears when filled
 var age = prompt('Quanti anni hai?', '30');
 console.log(age);
 document.getElementById('given-age').innerHTML = age;
@@ -17,7 +17,9 @@ console.log('total fare:', total_fare);
 
 /* trip fare if <18yo -> -20% discount
 else if >65yo -> 40% discount
-else -> standard fare */
+else -> standard fare
+-
+fares are rounded to the 2nd decimal */
 if(age < 18) {
   var fare_underage = total_fare - ((total_fare / 100) * 20);
   document.getElementById('final-fare').innerHTML = fare_underage.toFixed(2) + ' &euro;';
